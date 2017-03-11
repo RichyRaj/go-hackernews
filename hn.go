ca46da6b075ef0375f2c3a86c7ff9e3821c76554
Richard Raj <iamrichrock@gmail.com>
2017-03-11 21:00:23 +0530

    Fetch Stories Done

--- hn.go
+++ hn.go
@@ -8,16 +8,24 @@ import (
 
 var hnUrlBase = "https://hacker-news.firebaseio.com/v0/%s.json"
 
+var hnItemBase = "https://hacker-news.firebaseio.com/v0/item/%d.json"
+
+type Story struct {
+	Title string
+	Score int
+	By    string
+	Url   string
+}
+
 func GetLatest() string {
-	url := fmt.Sprintf(hnUrlBase, "newstories")
 	var items []int
+	url := fmt.Sprintf(hnUrlBase, "newstories")
 	fmt.Println(url)
 	resp, err := http.Get(url)
 	if err != nil {
 		return "error"
 	}
 	defer resp.Body.Close()
-	fmt.Println(resp.Body)
 	if resp.StatusCode != http.StatusOK {
 		return resp.Status + "Error"
 	}
@@ -27,6 +35,15 @@ func GetLatest() string {
 		return "JSON Parse Error"
 	}
 	fmt.Println(items)
+	items = items[:11]
+	for i := range items {
+		story := new(Story)
+		url = fmt.Sprintf(hnItemBase, items[i])
+		resp, err = http.Get(url)
+		// TODO : Error Handling
+		err = json.NewDecoder(resp.Body).Decode(story)
+		fmt.Println(story)
+	}
 	return "Done"
 
 }
